a7c9f7818dfba9e833dded7702b200403213f37e
io <yaroslavo@fivesysdev.com>
2018-11-20 10:45:38 +0200

    some changes in lesson 2

---
+++ lesson_2/include/stest.hpp
@@ -0,0 +1,12 @@
+#ifndef _CMAKE_EXAMPLE_2_H_
+#define _CMAKE_EXAMPLE_2_H_
+
+struct sTest final
+{
+  void print_i();
+  
+  int m_i { 0 };
+};
+
+
+#endif /* _CMAKE_EXAMPLE_2_H_ */
---
+++ lesson_2/src/stest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "stest.hpp"
+
+
+void sTest::print_i()
+{
+  std::cout << "m_i = " << m_i << std::endl;
+}
